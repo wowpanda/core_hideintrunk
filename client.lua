d9e0fcbc21731d7a05110e39fa9864289887c7bf
Johnny Pernik <40852281+johnnypernik@users.noreply.github.com>
2019-01-08 22:56:06 +0100

    Vehicle check, raise of distance, etc.

--- client.lua
+++ client.lua
@@ -8,15 +8,18 @@
 		    	player = PlayerPedId()
 			local plyCoords = GetEntityCoords(player, false)
 			local vehicle = VehicleInFront()
-  		
+
 		    if IsDisabledControlPressed(0, 19) and IsDisabledControlJustReleased(1, 44) and GetVehiclePedIsIn(player, false) == 0 and DoesEntityExist(vehicle) and IsEntityAVehicle(vehicle) then
 			 	SetVehicleDoorOpen(vehicle, 5, false, false)    	
 		    	if not inside then
 		        	AttachEntityToEntity(player, vehicle, -1, 0.0, -2.2, 0.5, 0.0, 0.0, 0.0, false, false, false, false, 20, true)		       		
-		       		if IsEntityAttached(player) and IsEntityAVehicle(vehicle) and not inside then
+		       		RaiseConvertibleRoof(vehicle, false)
+		       		if IsEntityAttached(player) then
 						SetTextComponentFormat("STRING")
-						AddTextComponentString('~INPUT_JUMP~ invisibility ~n~~s~~INPUT_CHARACTER_WHEEL~+~INPUT_COVER~ get out')
-						DisplayHelpTextFromStringLabel(0, 1, 1, -1)		       			
+						AddTextComponentString('~INPUT_JUMP~ neviditelnost ~n~~s~~INPUT_CHARACTER_WHEEL~+~INPUT_COVER~ vyskocit')
+						DisplayHelpTextFromStringLabel(0, 1, 1, -1)	
+						ClearPedTasksImmediately(player)
+						Citizen.Wait(100)	       			
 						TaskPlayAnim(player, 'timetable@floyd@cryingonbed@base', 'base', 1.0, -1, -1, 1, 0, 0, 0, 0)	
 		            	if not (IsEntityPlayingAnim(player, 'timetable@floyd@cryingonbed@base', 'base', 3) == 1) then
 		          			Streaming('timetable@floyd@cryingonbed@base', function()
@@ -31,17 +34,17 @@
 		    	elseif inside and IsDisabledControlPressed(0, 19) and IsDisabledControlJustReleased(1, 44) then
 		    		DetachEntity(player, true, true)
 		    		SetEntityVisible(player, true, true)
-		   			ClearPedTasks(player)  	  
+		   			ClearPedTasks(player)   
 		    		inside = false
-				ClearAllHelpMessages()		    		
+					ClearAllHelpMessages()		    	
 
 		    	end
 		    	Citizen.Wait(2000)
 		    	SetVehicleDoorShut(vehicle, 5, false)    	
 		    end
-	    	if DoesEntityExist(vehicle) and not inside then
+	    	if DoesEntityExist(vehicle) and IsEntityAVehicle(vehicle) and not inside and GetVehiclePedIsIn(player, false) == 0 then
 						SetTextComponentFormat("STRING")
-						AddTextComponentString('~INPUT_CHARACTER_WHEEL~~INPUT_COVER~ hide in')
+						AddTextComponentString('~n~~s~~INPUT_CHARACTER_WHEEL~+~INPUT_COVER~ hide in')
 						DisplayHelpTextFromStringLabel(0, 0, 1, -1)	
 			elseif DoesEntityExist(vehicle) and inside then
 		    		car = GetEntityAttachedTo(player)
@@ -50,12 +53,8 @@
 		   			DisableAllControlActions(0)
 		   			DisableAllControlActions(1)
 		   			DisableAllControlActions(2)
-					EnableControlAction(0, 289, true) --- F2	
-					EnableControlAction(0, 27, true) --- Up			
-					EnableControlAction(0, 173, true) --- Down		
-					EnableControlAction(0, 18, true) --- Enter	
 		   			EnableControlAction(0, 0, true) --- V - camera
-		   			EnableControlAction(0, 249, true) --- N - push to talk				
+		   			EnableControlAction(0, 249, true) --- N - push to talk	
 		   			EnableControlAction(2, 1, true) --- camera moving
 		   			EnableControlAction(2, 2, true) --- camera moving	
 		   			EnableControlAction(0, 177, true) --- BACKSPACE
@@ -70,8 +69,8 @@
 		    		DetachEntity(player, true, true)
 		    		SetEntityVisible(player, true, true)
 		   			ClearPedTasks(player)  	  
-		    		inside = false	
-				ClearAllHelpMessages()	
+		    		inside = false		
+		   			ClearAllHelpMessages()	 		    			
 			end  	
 	  	end
 	end)
@@ -91,27 +90,8 @@ function Streaming(animDict, cb)
 end	
 function VehicleInFront()
     local pos = GetEntityCoords(player)
-    local entityWorld = GetOffsetFromEntityInWorldCoords(player, 0.0, 3.5, 0.0)
+    local entityWorld = GetOffsetFromEntityInWorldCoords(player, 0.0, 6.0, 0.0)
     local rayHandle = CastRayPointToPoint(pos.x, pos.y, pos.z, entityWorld.x, entityWorld.y, entityWorld.z, 10, player, 0)
     local _, _, _, _, result = GetRaycastResult(rayHandle)
     return result
-end
-
-function DrawText3D2(x, y, z, text, scale)
-	local onScreen, _x, _y = World3dToScreen2d(x, y, z)
-	local pX, pY, pZ = table.unpack(GetGameplayCamCoords())
-
-	SetTextScale(scale, scale)
-	SetTextFont(4)
-	SetTextProportional(1)
-	SetTextEntry("STRING")
-	SetTextCentre(1)
-	SetTextColour(255, 255, 255, 215)
-
-	AddTextComponentString(text)
-	DrawText(_x, _y)
-
-	local factor = (string.len(text)) / 370
-
-	DrawRect(_x, _y + 0.0150, 0.030 + factor, 0.025, 41, 11, 41, 100)
 end
